3855ded350f95d31c9bd84f08ee62878c3b6fee4
badhaloninja <jacobpatrickcrawford@gmail.com>
2017-05-24 18:18:09 -0500

    Create KAAAH.user.js

---
+++ KAAAH.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name           KissAnime Ad Auto Hider
+// @match        *http://kissanime.ru/Anime/*
+// ==/UserScript==
+
+$('#adsIfrme6').remove();$(this).remove(); InitCloseButton();
+$('#adsIfrme7').remove();$(this).remove(); InitCloseButton();
+$('#adsIfrme8').remove();$(this).remove(); InitCloseButton();
+$('#adsIfrme10').remove();$(this).remove(); InitCloseButton();
+$('#adsIfrme11').remove();$(this).remove(); InitCloseButton();
