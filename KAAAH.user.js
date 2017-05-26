96ef58d56e39c01c5ff4f03e4ec24442220436b4
badhaloninja <jacobpatrickcrawford@gmail.com>
2017-05-25 20:50:48 -0500

    Update KAAAH.user.js

--- KAAAH.user.js
+++ KAAAH.user.js
@@ -1,10 +1,22 @@
 // ==UserScript==
-// @name           KissAnime Ad Auto Hider
-// @match        *http://kissanime.ru/Anime/*
+// @name           KissAnime Ad Hider
+// @author         badhaloninja
+// @include        *kissanime.ru/*
+// @namespace      https://github.com/badhaloninja/userscripttest
+// @version        0.3
+// @description    Automatically hides ads on KissAnime
+// @updateURL      https://github.com/badhaloninja/userscripttest/raw/master/KAAAH.user.js
+// @downloadURL    https://github.com/badhaloninja/userscripttest/raw/master/KAAAH.user.js
 // ==/UserScript==
 
+$('#adsIfrme1').remove();$(this).remove(); InitCloseButton();
+$('#adsIfrme2').remove();$(this).remove(); InitCloseButton();
+$('#adsIfrme3').remove();$(this).remove(); InitCloseButton();
+$('#adsIfrme4').remove();$(this).remove(); InitCloseButton();
+$('#adsIfrme5').remove();$(this).remove(); InitCloseButton();
 $('#adsIfrme6').remove();$(this).remove(); InitCloseButton();
 $('#adsIfrme7').remove();$(this).remove(); InitCloseButton();
 $('#adsIfrme8').remove();$(this).remove(); InitCloseButton();
+$('#adsIfrme9').remove();$(this).remove(); InitCloseButton();
 $('#adsIfrme10').remove();$(this).remove(); InitCloseButton();
 $('#adsIfrme11').remove();$(this).remove(); InitCloseButton();
